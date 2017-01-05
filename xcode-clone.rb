60a2ed9988eb20519c8584a16518be38ea95e5d1
Danny Nguyen <danny@80pct.com>
2017-01-05 16:09:16 -0500

    Implement most of initial script and rename

--- xcode-clone.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-# Steps:
-# - Create directory for unsigned clone
-# - Get list of items under /Applications/Xcode.app/Contents
-# - Symlink everything except /Applications/Xcode.app/Contents/MacOS
-# - Unsign Xcode
